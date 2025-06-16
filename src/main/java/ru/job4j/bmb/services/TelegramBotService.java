ad0b64056344806fd167e5167d7fdc3b793ddc36
Galeofan <20Andrew12@mail.ru>
2025-06-16 12:50:30 +0300

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -2,20 +2,42 @@ package ru.job4j.bmb.services;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
 @Service
-public class TelegramBotService {
+public class TelegramBotService implements BeanNameAware, ApplicationContextAware {
+
     private final BotCommandHandler handler;
+    private ApplicationContext applicationContext;
 
     public TelegramBotService(BotCommandHandler handler) {
         this.handler = handler;
     }
 
+    public void receive(Content content) {
+        handler.receive(content);
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("TelegramBotService bean name in context: " + name);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        System.out.println("ApplicationContext set in TelegramBotService");
+    }
+
     @PostConstruct
     public void init() {
         System.out.println("Bean TelegramBotService is going through init.");
+        displayAllBeanNames();
     }
 
     @PreDestroy
@@ -23,7 +45,11 @@ public class TelegramBotService {
         System.out.println("Bean TelegramBotService will be destroyed now.");
     }
 
-    public void receive(Content content) {
-        handler.receive(content);
+    public void displayAllBeanNames() {
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        System.out.println("Beans in ApplicationContext:");
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }
